caba795ac9ec653dd2100455a8b7b9696d5461ae
Jonathan Ross Hart <jonathan@jonathanrosshart.com>
2017-12-04 17:53:37 -0800

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name='kubernetes_workflow',
       author='Jonathan Ross Hart',
       author_email='jonathan@jonathanrosshart.com',
       license='Apache 2.0',
-      packages=['kubernetes_workflow'],
+      packages=['kubernetes_workflow',
+                'kubernetes_workflow.apis',
+                'kubernetes_workflow.models'],
       zip_safe=False)
